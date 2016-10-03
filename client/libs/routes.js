840e8d7519676d1ca521d580d8a0714541053e0e
GwFreak01 <gwfreak01@gmail.com>
2016-10-02 21:38:43 -0400

    Basic Routing works

---
+++ client/libs/routes.js
@@ -0,0 +1,31 @@
+/**
+ * Created by GWFreak01 on 10/2/16.
+ */
+
+FlowRouter.route('/', {
+    name: 'main',
+    action: function() {
+        BlazeLayout.render('MainLayout');
+    }
+});
+
+FlowRouter.route('/home', {
+    name: 'home',
+    action: function() {
+        BlazeLayout.render('HomeLayout');
+    }
+});
+
+FlowRouter.route('/login', {
+    name: 'login',
+    action: function() {
+        BlazeLayout.render('LoginLayout');
+    }
+});
+
+FlowRouter.route('/test', {
+    name: 'test',
+    action: function() {
+        BlazeLayout.render('LoginLayout', {main:'Test'});
+    }
+});
