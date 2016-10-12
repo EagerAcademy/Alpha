2d98c644023e353405d23ac2c5788a8b34235c8c
GwFreak01 <gwfreak01@gmail.com>
2016-10-11 20:59:54 -0400

    Stable Version. Testing DB

--- client/layouts/LoginLayout.js
+++ client/layouts/LoginLayout.js
@@ -1,8 +1,18 @@
 /**
  * Created by GWFreak01 on 10/11/16.
  */
-Meteor.subscribe('students');
 
-Template.LoginLayout.helper({
 
+Meteor.subscribe('Students');
+
+Template.LoginLayout.events({
+    'submit form': function (event) {
+        console.log("Init_Login");
+        event.preventDefault();
+        var usernameVar = event.target.username.value;
+        var passwordVar = event.target.password.value;
+        if (Student.findOne({email: usernameVar}, {password: passwordVar})){
+            FlowRouter.route('/home');
+        }
+    }
 });
